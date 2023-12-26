dcf8ff28ca4ecca5105d69230f91b312720a0e23
Orest Tsiatsiak <oresttsytsyak02@gmail.com>
2023-12-26 18:34:13 +0200

    Finished description of end points

--- src/main/java/com/duty/manager/controller/ExecutionFactController.java
+++ src/main/java/com/duty/manager/controller/ExecutionFactController.java
@@ -5,7 +5,15 @@ import com.duty.manager.dto.GetTestimonyDTO;
 import com.duty.manager.dto.RecordExecutionFactDTO;
 import com.duty.manager.service.ExecutionFactService;
 import com.duty.manager.service.TestimonyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +21,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-//TODO describe all endpoints with open api
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/execution-facts")
@@ -30,27 +38,81 @@ public class ExecutionFactController {
     private final TestimonyService testifyExecutionFact;
 
     @GetMapping(path = "/finished", params = {"from"})
+    @Operation(description = "Returns execution facts based on specified date range, \"finish date\" is used for search")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Default value for \"to\" is current day. The limit for maximal list size is 200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = GetExecutionFactDTO.class)
+                    )
+            )
+    )
     public List<GetExecutionFactDTO> getFinishedForDateRange(@RequestParam LocalDateTime from,
                                                              @RequestParam(required = false) LocalDateTime to) {
         return executionFactService.getFinishedForDateRange(from, to);
     }
 
     @PostMapping
-    public UUID recordExecutionFact(@RequestBody RecordExecutionFactDTO factDTO) {
-        return executionFactService.recordExecutionFact(factDTO);
+    @Operation(
+            description = "Records given execution fact."
+    )
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponse(
+            responseCode = "201",
+            description = "Returns id of execution fact that was recorded",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = UUID.class)
+            )
+    )
+    public ResponseEntity<UUID> recordExecutionFact(@RequestBody RecordExecutionFactDTO factDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED.value()).body(executionFactService.recordExecutionFact(factDTO));
     }
 
     @PostMapping("/finished")
+    @Operation(
+            description = "Sets finish time for execution fact with given id to current time."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Finish time was set successfully"
+    )
     public void finishExecution(@RequestBody UUID executionFactId) {
         executionFactService.finishExecution(executionFactId);
     }
 
     @PostMapping("/{executionFactId}/testimonies")
-    public UUID testifyExecutionFact(@PathVariable UUID executionFactId, Authentication authentication) {
-        return testifyExecutionFact.testifyExecutionFact(executionFactId, authentication);
+    @Operation(
+            description = "Authenticated person testifies execution fact."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Execution fact was successfully testified. It returns id of created testimony",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = UUID.class)
+            )
+    )
+    public ResponseEntity<UUID> testifyExecutionFact(@PathVariable UUID executionFactId, Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.CREATED.value())
+                .body(testifyExecutionFact.testifyExecutionFact(executionFactId, authentication));
     }
 
     @GetMapping("/{executionFactId}/testimonies")
+    @Operation(description = "Returns testimonies for specified execution fact.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Returns testimonies for specified execution fact. Default value for \"page\" is 0," +
+                    " \"pageSize\" is 200. Min value for page is 0, Max value for page size is 200.",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = GetTestimonyDTO.class)
+                    )
+            )
+    )
     public List<GetTestimonyDTO> getTestimoniesForExecutionFact(@PathVariable UUID executionFactId,
                                                                 @RequestParam(required = false, defaultValue = "0")
                                                                 Integer page,
@@ -60,6 +122,18 @@ public class ExecutionFactController {
     }
 
     @GetMapping(path = "/finished", params = {"from", "executorId"})
+    @Operation(description = "Returns execution facts based on specified date range and for specific executor," +
+            " \"finish date\" is used for search")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Default value for \"to\" is current day. The limit for maximal list size is 200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = GetExecutionFactDTO.class)
+                    )
+            )
+    )
     public List<GetExecutionFactDTO> getFinishedForDateRangeForExecutor(@RequestParam LocalDateTime from,
                                                                         @RequestParam(required = false) LocalDateTime to,
                                                                         @RequestParam UUID executorId) {
@@ -67,12 +141,36 @@ public class ExecutionFactController {
     }
 
     @GetMapping(path = "/active", params = {"from"})
+    @Operation(description = "Returns execution facts based on specified date range," +
+            " \"start date\" is used for search, ignores all records that have \"finish time\"")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Default value for \"to\" is current day. The limit for maximal list size is 200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = GetExecutionFactDTO.class)
+                    )
+            )
+    )
     public List<GetExecutionFactDTO> getActiveForDateRange(@RequestParam LocalDateTime from,
                                                            @RequestParam(required = false) LocalDateTime to) {
         return executionFactService.getActiveForDateRange(from, to);
     }
 
     @GetMapping(path = "/active", params = {"from", "executorId"})
+    @Operation(description = "Returns execution facts based on specified date range and for specific executor," +
+            " \"start date\" is used for search, ignores all records that have \"finish time\"")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Default value for \"to\" is current day. The limit for maximal list size is 200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = GetExecutionFactDTO.class)
+                    )
+            )
+    )
     public List<GetExecutionFactDTO> getActiveForDateRangeForExecutor(@RequestParam LocalDateTime from,
                                                                       @RequestParam(required = false) LocalDateTime to,
                                                                       @RequestParam UUID executorId) {
--- src/main/java/com/duty/manager/controller/ParticipantController.java
+++ src/main/java/com/duty/manager/controller/ParticipantController.java
@@ -1,9 +1,18 @@
 package com.duty.manager.controller;
 
+import com.duty.manager.dto.GetDutyDTO;
 import com.duty.manager.dto.GetParticipantDTO;
 import com.duty.manager.dto.RegisterParticipantDTO;
 import com.duty.manager.service.ParticipantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,16 +32,57 @@ public class ParticipantController {
     private final ParticipantService participantService;
 
     @PostMapping
-    public UUID register(@RequestBody RegisterParticipantDTO participantDTO) {
-        return participantService.registerParticipant(participantDTO);
+    @Operation(
+            description = "Register participant."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Participant was successfully registered. It returns id of created participant",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = UUID.class)
+            )
+    )
+    public ResponseEntity<UUID> register(@RequestBody RegisterParticipantDTO participantDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED.value())
+                .body( participantService.registerParticipant(participantDTO));
     }
 
     @GetMapping("/{identifier}")
+    @Operation(description = "Returns participant by specified id or email. Returns 404 if no participant was found")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Returns participant with specified id or email",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = GetDutyDTO.class)
+            )
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Participant with specified id or email was not found",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = ExceptionResponse.class)
+            )
+    )
     public GetParticipantDTO getParticipant(@PathVariable String identifier) {
         return participantService.getParticipant(identifier);
     }
 
     @GetMapping
+    @Operation(description = "Returns list of registered participants")
+    @ApiResponse(
+            responseCode = "200",
+            description = "You are allowed to get this list, defaults: page = 0, pageSize = 200," +
+                    " restrictions: page min = 0, pageSize max = 200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(
+                            schema = @Schema(implementation = GetParticipantDTO.class)
+                    )
+            )
+    )
     public List<GetParticipantDTO> getParticipants(@RequestParam(required = false, defaultValue = "0")
                                                    Integer page,
                                                    @RequestParam(required = false, defaultValue = "200")
